de485a40409b1eb3ec3323e376f8f29ad07e03e2
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-04-17 16:42:11 -0400

    Adding a database Mongodb

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ You also need a `docker-compose.yml` file with the following content
 ```
 version: '3'
 services:
+  db:
+    image: mongo:4.2.8
+    container_name: db
+    ports:
+      - "4000:27017"
+    restart: always
   azure-storage:
     image: azure-storage
     build: 
@@ -30,11 +36,11 @@ services:
     container_name: video-storage
 
     ports: 
-     - "4000:80"
+     - "4001:80"
     environment:
       - PORT=80
-      - STORAGE_ACCOUNT_NAME=<your storage azure, aws, etc>
-      - STORAGE_ACCESS_KEY=<your access key>
+      - STORAGE_ACCOUNT_NAME=<your-storage-name>
+      - STORAGE_ACCESS_KEY=<your-storage-access-key>
     restart: "no"
   video-streaming:
     image: video-streaming
@@ -43,9 +49,11 @@ services:
       dockerfile: Dockerfile
     container_name: video-streaming
     ports:
-      - "4001:80"
+      - "4002:80"
     environment:
       - PORT=80
+      - DBHOST=mongodb://db:27017
+      - DBNAME=video-streaming
       - VIDEO_STORAGE_HOST=video-storage
       - VIDEO_STORAGE_PORT=80
     restart: "no"
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,30 +1,64 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 const app = express();
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_HOST);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-app.get("/", (req, res) => {
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: "/video?path=test-video.mp4",
-      method: "GET",
-      headers: req.headers,
-    },
-    (forwardResponse) => {
-      res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-      forwardResponse.pipe(res);
-    }
-  );
+function main() {
+  return mongodb.MongoClient.connect(DBHOST).then((client) => {
+    const db = client.db(DBNAME);
+    const videosCollection = db.collection("videos");
 
-  req.pipe(forwardRequest);
-});
+    app.get("/video", (req, res) => {
+      const videoId = new mongodb.ObjectId(req.query.id);
+      videosCollection
+        .findOne({ _id: videoId })
+        .then((videoRecord) => {
+          if (!videoRecord) {
+            res.sendStatus(404);
+            return;
+          }
+          console.log("working here!");
+          const forwardRequest = http.request(
+            {
+              host: VIDEO_STORAGE_HOST,
+              port: VIDEO_STORAGE_PORT,
+              path: `/video?path=${videoRecord.videoPath}`,
+              method: "GET",
+              headers: req.headers,
+            },
+            (forwardResponse) => {
+              res.writeHeader(
+                forwardResponse.statusCode,
+                forwardResponse.headers
+              );
+              forwardResponse.pipe(res);
+            }
+          );
+          req.pipe(forwardRequest);
+        })
+        .catch((err) => {
+          console.log("Database query failed.");
+          console.log((err && err.stack) || err);
+          res.sendStatus(500);
+        });
+    });
 
-app.listen(PORT, () => {
-  console.log(`Microservice online`);
-});
+    app.listen(PORT, () => {
+      console.log(`Microservice online.`);
+    });
+  });
+}
+
+main()
+  .then(() => console.log("Microservice online."))
+  .catch((err) => {
+    console.log("Microservice failed to start");
+    console.log((err && err.stack) || err);
+  });
